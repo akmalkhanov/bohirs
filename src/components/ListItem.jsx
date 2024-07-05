bb6dcd4e1c2cd700c29f3b44ec15b7e62ef6fb02
akmalkhanov <138787426+akmalkhanov@users.noreply.github.com>
2024-07-05 18:41:43 +0500

    init

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,23 @@
+import { FaTrashAlt } from "react-icons/fa";
+
+const ListItem = ({ item, handleDelete }) => {
+  return (
+    <li className="item">
+      <input type="checkbox" checked={item.checked} />
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => {}}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role="button"
+        tabIndex="0"
+        aria-label={`Delete ${item.item}`}
+      />
+    </li>
+  );
+};
+
+export default ListItem;
